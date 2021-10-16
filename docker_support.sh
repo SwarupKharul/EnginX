ce8360e9d2ab602af90da0dffa956a47dab0b142
Swarup Kharul <swarupkharulsk@gmail.com>
2021-10-16 17:15:35 +0530

    fix: rename functions for docker

--- docker_support.sh
+++ docker_support.sh
@@ -6,29 +6,8 @@ LOGGER="logger"
 
 DOCKER_PORT=8000
 
-# Start docker
-function start_docker() {
-    # if docker is not installed then install it
-    if ! command -v docker >/dev/null 2>&1; then
-        e_warning "Docker is not installed. Installing it now..."
-        sudo apt install -y docker
-        e_success "Docker is installed."
-    fi
-
-    # if docker-compose is not installed then install it
-    if ! command -v docker-compose >/dev/null 2>&1; then
-        e_warning "Docker-compose is not installed. Installing it now..."
-        sudo apt install -y docker-compose
-        e_success "Docker-compose is installed."
-    fi
-    # Start docker
-    e_arrow "Starting docker...."
-    sudo service docker start
-    e_success "Docker started"
-}
-
 # Build docker image
-function build_docker_image() {
+function docker_build_image() {
     # check if docker-compose.yml exists
     if [ -f "docker-compose.yml" ]; then
         # Build docker image
@@ -42,10 +21,32 @@ function build_docker_image() {
         sudo docker build -t ${DOCKER_IMAGE} .
         e_success "Docker image built"
     fi
-
 }
 
-function run_docker_image(){
+# Start docker
+function docker_start() {
+    # if docker is not installed then install it
+    if ! command -v docker >/dev/null 2>&1; then
+        e_warning "Docker is not installed. Installing it now..."
+        sudo apt install -y docker
+        e_success "Docker is installed."
+    fi
+
+    # if docker-compose is not installed then install it
+    if ! command -v docker-compose >/dev/null 2>&1; then
+        e_warning "Docker-compose is not installed. Installing it now..."
+        sudo apt install -y docker-compose
+        e_success "Docker-compose is installed."
+    fi
+
+    # Start docker
+    e_arrow "Starting docker...."
+    sudo service docker start
+    e_success "Docker started"
+    docker_build_image
+}
+
+function docker_run(){
     if [ -f "docker-compose.yml" ]; then
         # Run docker image
         e_arrow "Running docker image...."
